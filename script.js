8bd3b142b32ef0cebd2e418ea14362416c3d906d
harsh singh <hrs24182418@gmail.com>
2026-03-16 09:24:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,148 @@
+//================================
+// Weather Tracker App
+//================================  
+const API_KEY = "dc1adf0a01f2ea6709aaf0a92c9a7d37"; 
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+const weatherBox = document.getElementById("weatherResult");
+const historyBox = document.getElementById("history");
+const logBox = document.getElementById("logBox");
+
+// ================================
+// Console Logger (Event Loop Demo)
+// ================================
+function log(message) {
+    console.log(message);
+    logBox.textContent += message + "\n";
+}
+
+// ================================
+// Fetch Weather (Async /Await)
+// ================================
+async function getWeather(city) {
+    log("Function Started");
+    log("Before fetch()");
+
+    try {
+        const response = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        log("After fetch() - Response Received");
+
+        if (!response.ok) {
+            throw new Error("Invalid city name");
+        }
+        const data = await response.json();
+        log("JSON Parsed");
+
+        displayWeather(data);
+        saveCity(city);
+    } catch (error) {
+        log("Error: " + error.message);
+        showError("City not found or Network error");
+    }
+    log("Function Ended");
+}
+
+// ================================
+// Promise Version (.then/ .catch)
+// ================================
+function getWeatherUsingPromise(city) {
+    log("Promise Version Started");
+
+    fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Invalid city");
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayWeather(data);
+            saveCity(city);
+        })
+        .catch(error => {
+            log("Promise Error: " + error.message);
+            showError("Something went wrong");
+        });
+}
+
+// ================================
+// Display Weather
+//=================================
+function displayWeather(data) {
+
+    weatherBox.innerHTML = `
+           <p><strong>City:</strong> ${data.name}</p>
+           <p><strong>Temperature:</strong> ${data.main.temp} °C</p>
+           <p><strong>Weather:</strong> ${data.weather[0].description}</p>
+           <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
+              <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
+    `;
+}
+
+// ================================
+// Show Error
+// ================================
+function showError(message) {
+    weatherBox.innerHTML = `<span style="color: red;">${message}</span>`;
+}
+
+// ================================
+// Local Storage - Save City
+// ================================
+function saveCity(city) {
+
+    let cities = JSON.parse(localStorage.getItem("weatherCities")) || [];
+
+    if (!cities.includes(city)) {
+        cities.push(city);
+        localStorage.setItem("weatherCities", JSON.stringify(cities));
+    }
+    loadHistory();
+}
+
+//===============================
+// Load Search History
+//===============================
+function loadHistory() {
+
+    historyBox.innerHTML = "";
+
+    let cities = JSON.parse(localStorage.getItem("weatherCities")) || [];
+
+    cities.forEach(city => {
+        const btn = document.createElement("button");
+        btn.textContent = city;
+
+        btn.addEventListener("click", () => {
+            getWeather(city);
+        });
+        historyBox.appendChild(btn);
+    });
+}
+
+// ================================
+// Search Button Event
+// ================================
+searchBtn.addEventListener("click", () => {
+    const city = cityInput.value.trim();
+
+    if (!city) {
+        showError("Please enter a city name");
+        return;
+    }
+    log("Button Clicked");
+
+    getWeather(city);
+
+    cityInput.value = "";
+});
+
+// ================================
+// On Page Load
+// ================================
+window.addEventListener("load", () => {
+    log("Page Loaded");
+    loadHistory();
+});
